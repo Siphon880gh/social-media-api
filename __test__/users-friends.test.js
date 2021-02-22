b92d7cf442055340d1a9ce041d22638cea426ed4
Weng Fei Fung <weffung@ucdavis.edu>
2021-02-22 04:27:04 -0800

    Test: Friends

--- __test__/users-friends.test.js
+++ __test__/users-friends.test.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const { Schema, Types } = mongoose;
 var User = require('../models/User');
 var Thought = require('../models/Thought');
 
@@ -47,10 +47,46 @@ describe("Test User", () => {
         // If you had used deleteOne, then the resolved data is { deleteUserInfo: { n: 1, ok: 1, deletedCount: 1 } }
         // Using findOneAndDelete, then the resolved data is the row that's deleted
         expect(retUsers.length).toEqual(3);
-        await User.findOneAndDelete({ username: "test3" }).then(deleteUserInfo => { console.log({ deleteUserInfo }); });
+        await User.findOneAndDelete({ username: "test3" }).then(deleteUserInfo => { /* console.log({ deleteUserInfo }); */ });
         retUsers = await User.find({});
         expect(retUsers.length).toEqual(2);
-        console.log({ retUsers });
+        // console.log({ retUsers });
+    });
+});
+
+describe("Test Friend", () => {
+    beforeAll(async function() {
+
+        retUsers = await User.find({})
+            .select("-__v")
+            .populate({ path: "thoughts", select: "-__v" })
+            .populate({ path: "friends", select: "-__v" });
+    })
+    test("Testing friends", async function() {
+
+        // Get ID
+        let { _id } = await User.findOne({ username: "test2" });
+        let newFriendId = _id;
+
+        // See if is a valid object Id
+        // expect(Types.ObjectId.isValid(newFriendId)).toEqual(true);
+
+        // See if is a valid object Id: This also works
+        // If a string is ObjectId, casting it to ObjectId will not change the string
+        expect(newFriendId).toEqual(Types.ObjectId(newFriendId));
+
+        await User.findOneAndUpdate({ username: "test" }, {
+            $push: { friends: newFriendId }
+        })
+
+        // // Test that the user has a new friend
+        let himAndHisFriend = await User.findOneAndUpdate({ username: "test" })
+            .populate({ path: "friends", select: "-__v" })
+            // expect(retUsers).toEqual(expect.any(Object));
+            // expect(retUsers.length).toEqual(3);
+            // expect(retUsers[0].username).toEqual("test");
+            // console.log({ retUsers });
+        console.log({ himAndHisFriend: JSON.stringify(himAndHisFriend) });
     });
 });
 
