89f5ec2f1309daa126b80c5bc6033126fbd00033
Weng Fei Fung <weffung@ucdavis.edu>
2021-02-23 07:34:05 -0800

    Refactored Test into Route: GET a single user by its _id and populate its thought and friend data

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,12 +2,15 @@ var app = require("express");
 var router = app.Router();
 var { User, Thought } = require("../../models");
 
+
+// GET all users
 router.get("/", async(req, res) => {
     let retUsers = await User.find({}).select("-__v").populate({ path: "thoughts", select: "-__v" }).populate({ path: "friends", select: "-__v" });
     // let retUsers = await User.find({});
     res.json(retUsers);
 });
 
+// POST a new user
 router.post("/", async(req, res) => {
     // Create new user testUserNewByRoute
     let retUser = await User.create({ username: req.body.username, email: req.body.email }).catch(err => {
@@ -16,6 +19,14 @@ router.post("/", async(req, res) => {
         }
         res.status(500).json({ message: err, error: 1 });
     })
+    res.json(retUser);
+});
+
+// GET a single user by its \_id and populate its thought and friend data
+router.get("/:userId", async(req, res) => {
+    let retUser = await User.findOne({
+        _id: req.params.userId
+    }).populate({ path: "thoughts", select: "-__v" }).populate({ path: "friends", select: "-__v" }).select("-__v");
     res.json(retUser);
 });
 
