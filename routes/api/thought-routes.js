0fdcbe4a3438728322172b98a75cc0e9103c7013
Weng Fei Fung <weffung@ucdavis.edu>
2021-02-23 22:59:43 -0800

    Better commenting: Added request method and API endpoint in comment next to routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -3,6 +3,7 @@ var router = app.Router();
 var { User, Thought } = require("../../models");
 
 // GET to get all thoughts
+// GET /api/thoughts
 router.get("/", async(req, res) => {
     let retThought = await Thought.find({});
     res.json(retThought);
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,6 +3,7 @@ var router = app.Router();
 var { User, Thought } = require("../../models");
 
 // GET all users
+// GET /api/users
 router.get("/", async(req, res) => {
     let retUsers = await User.find({}).select("-__v").populate({ path: "thoughts", select: "-__v" }).populate({ path: "friends", select: "-__v" });
     // let retUsers = await User.find({});
@@ -10,6 +11,7 @@ router.get("/", async(req, res) => {
 });
 
 // POST a new user
+// POST /api/users
 router.post("/", async(req, res) => {
     // Create new user testUserNewByRoute
     let retUser = await User.create({ username: req.body.username, email: req.body.email }).catch(err => {
@@ -24,6 +26,7 @@ router.post("/", async(req, res) => {
 });
 
 // GET a single user by its \_id and populate its thought and friend data
+// GET /api/users/:userId
 router.get("/:userId", async(req, res) => {
     let retUser = await User.findOne({
         _id: req.params.userId
@@ -32,6 +35,7 @@ router.get("/:userId", async(req, res) => {
 });
 
 // PUT to update a user by its _id
+// PUT /api/users/:userId
 router.put("/:userId", async(req, res) => {
     let retUser = await User.findOneAndUpdate({
         _id: req.params.userId
@@ -40,6 +44,7 @@ router.put("/:userId", async(req, res) => {
 });
 
 // DELETE to remove user by its _id
+// DELETE /api/users/:userId
 router.delete("/:userId", async(req, res) => {
     let retUser = await User.findOneAndDelete({
         _id: req.params.userId
@@ -48,12 +53,14 @@ router.delete("/:userId", async(req, res) => {
 });
 
 // POST to add a new friend to a user's friend list
+// POST /api/users/:userId/friends/:friendId
 router.post("/:userId/friends/:friendId", async(req, res) => {
     let retUser = await User.findOneAndUpdate({ _id: req.params.userId }, { $push: { friends: req.params.friendId } }, { new: true }).populate({ path: "thoughts", select: "-__v" }).populate({ path: "friends", select: "-__v" }).select("-__v");
     res.json(retUser);
 });
 
 // DELETE to remove a friend from a user's friend list
+// DELETE /api/users/:userId/friends/:friendId
 router.delete("/:userId/friends/:friendId", async(req, res) => {
     let retUser = await User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { new: true }).populate({ path: "thoughts", select: "-__v" }).populate({ path: "friends", select: "-__v" }).select("-__v");
     res.json({ message: "Deleted friend and updated the friend list of the associated user", user: retUser });
